6cab3abaef46ca2976228f1e0807926a7be5e5ec
Leonard Garvey <lengarvey@gmail.com>
2013-04-22 14:08:32 +1000

    adding comments resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 QuickBlog::Application.routes.draw do
+  resources :comments
+
+
   resources :posts
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130422002523) do
+ActiveRecord::Schema.define(:version => 20130422014740) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "post_id"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
 
   create_table "posts", :force => true do |t|
     t.string   "title"
