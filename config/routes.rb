c0d2da294bd68d65a7f075cc3b63335a1a064ace
Leonard Garvey <lengarvey@gmail.com>
2013-04-23 12:10:49 +1000

    Adding a default root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 QuickBlog::Application.routes.draw do
+  root :to => 'posts#index'
+
   resources :posts do
     resources :comments, :only => [:create]
   end
-
-  # root :to => 'welcome#index'
 end
-
-
